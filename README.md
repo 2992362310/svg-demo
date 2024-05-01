6349bde400daf592e3ca0f906a0826f7ecf6b0c6
GDO <2992362310@qq.com>
2024-05-01 22:31:44 +0800

    feat:节点交互测试代码

--- README.md
+++ README.md
@@ -27,3 +27,29 @@ pnpm dev
 ```sh
 pnpm build
 ```
+
+## TODO
+- [ - ] 画板
+- [ - ] 单个节点交互，拖动、编辑
+- [  ] 新增节点
+- [  ] 节点连线
+
+## 参考
+- [kitymind](https://www.jyshare.com/more/kitymind/index.html)
+- [antv g6](https://g6.antv.vision/examples/case/treeDemos#mindmap)
+- [svg 贝塞尔曲线](https://www.zhangxinxu.com/wordpress/2014/06/deep-understand-svg-path-bezier-curves-command/)
+- [svg 贝塞尔曲线](https://developer.mozilla.org/zh-CN/docs/Web/SVG/Tutorial/Paths)
+- [excalidraw](https://excalidraw.com/)
+
+## 库
+- [svg.js](https://svgjs.dev/docs/3.0/)
+
+## 实现过程关键点
+1. 画布无线滚动与缩放,svg缩放时html元素scale
+2. 节点拖动与点击
+
+
+# 明天
+1. 梳理下思路和代码
+2. 做个配置面板
+3. 
--- src/App.vue
+++ src/App.vue
@@ -1,188 +1,302 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import { SVG } from "@svgdotjs/svg.js";
-import "@svgdotjs/svg.panzoom.js";
-import "@svgdotjs/svg.draggable.js";
-import { useEventListener, useKeyModifier } from "@vueuse/core";
-import { useMove } from "./useMove";
+  import { onMounted, ref } from 'vue';
+  import { SVG } from '@svgdotjs/svg.js';
+  import '@svgdotjs/svg.panzoom.js';
+  import '@svgdotjs/svg.draggable.js';
+  import { useEventListener, useKeyModifier } from '@vueuse/core';
+  import { useMove } from './useMove';
+  import IconCommunity from './components/icons/IconCommunity.vue';
+  import IconTooling from './components/icons/IconTooling.vue';
 
-const controlState = useKeyModifier("Control", { events: ["mousewheel"] });
+  const controlState = useKeyModifier('Control', { events: ['mousewheel'] });
 
-const editing = ref(false);
-const editingText = defineModel();
+  const editing = ref(false);
+  const editingText = defineModel();
+  const editX = ref(0);
+  const editY = ref(0);
+  const inputRef = ref(null);
 
-const svgRef = ref(null);
-let draw = null;
-let text = null;
-let rect = null;
-let zoom = 1;
-const fontSize = 40;
+  const showBtn = ref(true);
 
-const x = ref(0);
-const y = ref(0);
-const width = ref(window.innerWidth);
-const height = ref(window.innerHeight);
+  let isMoving = false;
 
-onMounted(() => {
-  draw = new SVG(svgRef.value);
+  const svgRef = ref(null);
+  let draw = null;
+  let text = null;
+  let rect = null;
+  let zoom = 1;
+  const fontSize = 40;
 
-  drawRect();
-});
+  const x = ref(0);
+  const y = ref(0);
+  const width = ref(window.innerWidth);
+  const height = ref(window.innerHeight);
 
-const drawRect = () => {
-  const options = {
-    x: width.value / 2 - 50,
-    y: height.value / 2 - 50,
-    width: 200,
-    height: 100,
+  onMounted(() => {
+    draw = new SVG(svgRef.value);
+
+    const startX = 100;
+    const startY = height.value / 2 + 100;
+    const endX = 800;
+    const endY = 100;
+
+    drawRect(startX, startY);
+
+    drawRect(endX, endY);
+
+    const start = {
+      x: startX + 250,
+      y: startY + 40,
+    };
+
+    const end = {
+      x: endX,
+      y: endY + 40,
+    };
+
+    const centerX = (start.x + end.x) / 2;
+
+    // 曲线
+    const svgPath2 = `
+    M ${start.x} ${start.y}
+    C ${start.x + centerX / 2} ${start.y}
+    ${endX - centerX / 3} ${end.y}
+    ${end.x} ${end.y}
+    `;
+
+    // 直角
+    const svgPath1 = `M ${start.x} ${start.y} H ${(start.x + end.x) / 2} V ${
+      end.y
+    } L ${end.x} ${end.y}`;
+
+    const path = draw.path(svgPath2).fill('none');
+    path.stroke({
+      color: '#f06',
+      width: 4,
+      linecap: 'round',
+      linejoin: 'round',
+    });
+  });
+
+  const drawRect = (pX, pY) => {
+    const options = {
+      x: pX,
+      y: pY,
+      width: 200,
+      height: 100,
+    };
+
+    rect = draw.rect(options);
+    rect.stroke('#E6A23C').fill('none');
+
+    text = draw.plain('hello world');
+    text
+      .dx(options.x + fontSize / 2)
+      .dy(options.y + fontSize * 1.5)
+      .font({ fill: '#f06', family: 'Inconsolata', size: fontSize });
+
+    rect.width(text.length() + fontSize);
+    rect.height(fontSize + fontSize);
+
+    // setTimeout(() => {
+    //   text.plain("hello world again");
+    //   rect.width(text.length() + fontSize);
+    // }, 3000);
+
+    var group = draw.group().draggable();
+    group.add(rect);
+    group.add(text);
+
+    group.on('mousedown', (e) => {
+      e.preventDefault();
+      isMoving = false;
+    });
+
+    group.on('mousemove', (e) => {
+      e.preventDefault();
+      isMoving = true;
+    });
+
+    group.on('mouseup', (e) => {
+      // e.preventDefault();
+      // e.stopPropagation();
+
+      if (isMoving) return;
+
+      editingText.value = text.node.textContent;
+      editing.value = true;
+      // text.plain('');
+
+      const { x, y } = text.node.getBoundingClientRect();
+      editX.value = x;
+      editY.value = y;
+
+      console.log(x, y, zoom);
+
+      setTimeout(() => {
+        inputRef.value.focus();
+      }, 0);
+    });
+
+    group.on('mouseover', (e) => {
+      // e.preventDefault();
+      // e.stopPropagation();
+
+      rect.fill('#FFC300');
+    });
+
+    group.on('mouseout', (e) => {
+      e.preventDefault();
+      // e.stopPropagation();
+      rect.fill('#E6A23C');
+    });
   };
 
-  rect = draw.rect(options);
-  rect.stroke("#E6A23C").fill("none");
+  const updateViewBox = () => {
+    // console.log("updateViewBox");
+    width.value = window.innerWidth;
+    height.value = window.innerHeight;
+  };
 
-  text = draw.plain("hello world");
-  text
-    .dx(options.x + fontSize / 2)
-    .dy(options.y + fontSize * 1.5)
-    .font({ fill: "#f06", family: "Inconsolata", size: fontSize });
+  // 上下滚动
+  const onMouseWheel = (event) => {
+    event.preventDefault();
 
-  rect.width(text.length() + fontSize);
-  rect.height(fontSize + fontSize);
+    if (controlState.value) {
+      handleZoom(event);
+    } else {
+      const { deltaY } = event;
+      y.value += deltaY;
+    }
+  };
 
-  // setTimeout(() => {
-  //   text.plain("hello world again");
-  //   rect.width(text.length() + fontSize);
-  // }, 3000);
+  const handleZoom = (event) => {
+    // 0.2 3
+    const { deltaY, clientX, clientY } = event;
+    zoom += +(-deltaY / 1000).toFixed(1);
 
-  var group = draw.group().draggable();
-  group.add(rect);
-  group.add(text);
+    if (zoom > 0.3 && zoom < 3) {
+      draw.zoom(zoom, { x: clientX, y: clientY });
 
-  text.on("click", () => {
-    editingText.value = text.node.textContent;
-    editing.value = true;
-    text.plain("");
-  });
+      const viewBox = draw.viewbox();
+      x.value = viewBox.x;
+      y.value = viewBox.y;
+      width.value = viewBox.width;
+      height.value = viewBox.height;
+    }
 
-  text.on("mouseover", (e) => {
-    e.preventDefault();
-    rect.stroke("#FFC300");
-  });
+    zoom = zoom < 0.3 ? 0.3 : zoom > 3 ? 3 : zoom;
+  };
 
-  text.on("mouseout", (e) => {
-    e.preventDefault();
-    rect.stroke("#E6A23C");
-  });
-};
+  const handleEnter = () => {
+    // console.log("handleEnter");
+    text.plain(editingText.value);
+    rect.width(text.length() + fontSize);
+    editing.value = false;
+  };
 
-const updateViewBox = () => {
-  // console.log("updateViewBox");
-  width.value = window.innerWidth;
-  height.value = window.innerHeight;
-};
+  useEventListener(window, 'resize', updateViewBox);
 
-// 上下滚动
-const onMouseWheel = (event) => {
-  event.preventDefault();
+  useEventListener(svgRef, 'mousewheel', onMouseWheel);
 
-  if (controlState.value) {
-    handleZoom(event);
-  } else {
-    const { deltaY } = event;
-    y.value += deltaY;
-  }
-};
-
-const handleZoom = (event) => {
-  // 0.2 3
-  const { deltaY, clientX, clientY } = event;
-  zoom += +(-deltaY / 1000).toFixed(1);
-
-  if (zoom > 0.5 && zoom < 3) {
-    draw.zoom(zoom, { x: clientX, y: clientY });
-
-    const viewBox = draw.viewbox();
-    x.value = viewBox.x;
-    y.value = viewBox.y;
-    width.value = viewBox.width;
-    height.value = viewBox.height;
-  }
-};
-
-const handleEnter = () => {
-  // console.log("handleEnter");
-  text.plain(editingText.value);
-  rect.width(text.length() + fontSize);
-  editing.value = false;
-};
-
-useEventListener(window, "resize", updateViewBox);
-
-useEventListener(svgRef, "mousewheel", onMouseWheel);
-
-useMove(svgRef, x, y);
+  useMove(svgRef, x, y);
 </script>
 
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    ref="svgRef"
-    class="svg-container"
-    :viewBox="`${x} ${y} ${width} ${height}`"
-  >
-    <defs>
-      <pattern
-        id="gridPattern"
-        patternUnits="userSpaceOnUse"
-        x="0"
-        y="0"
-        width="20"
-        height="20"
-      >
-        <path d="M 0 0 L 0 20" stroke="gray" stroke-width="0.5" />
-        <path d="M 0 0 L 20 0" stroke="gray" stroke-width="0.5" />
-      </pattern>
-    </defs>
+  <div class="svg-demo-wrap">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      ref="svgRef"
+      class="svg-container"
+      :viewBox="`${x} ${y} ${width} ${height}`"
+    >
+      <defs>
+        <pattern
+          id="gridPattern"
+          patternUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="20"
+          height="20"
+        >
+          <path d="M 0 0 L 0 20" stroke="gray" stroke-width="0.5" />
+          <path d="M 0 0 L 20 0" stroke="gray" stroke-width="0.5" />
+        </pattern>
+      </defs>
 
-    <rect
-      :x="x"
-      :y="y"
-      :width="width"
-      :height="height"
-      fill="url(#gridPattern)"
-    />
+      <rect
+        :x="x"
+        :y="y"
+        :width="width"
+        :height="height"
+        fill="url(#gridPattern)"
+      />
+    </svg>
 
-    <foreignObject
-      x="500"
-      y="500"
-      overflow="visible"
-      width="200"
-      height="60"
-      requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+    <label
+      :style="{
+        left: editX + 'px',
+        top: editY + 'px',
+        transform: `scale(${zoom})`,
+      }"
       v-show="editing"
     >
       <input
+        ref="inputRef"
         type="text"
-        style="
-          padding: 10px;
-          border: 1px solid red;
-          background-color: white;
-          opacity: 0.5;
-          font-family: Verdana;
-          font-size: 20px;
-          white-space: pre;
-          word-wrap: normal;
-          overflow: visible;
-          overflow-y: visible;
-          overflow-x: visible;
-        "
         @keyup.enter="handleEnter"
-        contentEditable="true"
-        xmlns="http://www.w3.org/1999/xhtml"
+        @blur="handleEnter"
         v-model="editingText"
       />
-    </foreignObject>
-  </svg>
+    </label>
+
+    <ul class="btn-wrap" v-show="showBtn">
+      <IconCommunity />
+      <IconTooling />
+
+      <li>配置项</li>
+
+      <li>删除节点,所有子节点一起删除</li>
+      <li>添加子节点</li>
+      <li>添加同级节点</li>
+    </ul>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .svg-demo-wrap {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  .svg-demo-wrap > label {
+    position: absolute;
+    transform-origin: 0 0;
+  }
+
+  .svg-demo-wrap > label > input {
+    padding: 10px;
+    border: 1px solid red;
+    background-color: white;
+    /* opacity: 0.5; */
+    font-family: Verdana;
+    font-size: 20px;
+    white-space: pre;
+    word-wrap: normal;
+    overflow: visible;
+    overflow-y: visible;
+    overflow-x: visible;
+  }
+
+  .svg-demo-wrap > .btn-wrap {
+    /* list-style: none; */
+    padding: 10px 10px 10px 20px;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    width: 100px;
+    height: fit-content;
+    background: pink;
+  }
+</style>
